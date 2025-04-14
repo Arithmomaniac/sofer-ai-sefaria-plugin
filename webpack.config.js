7089c25f0228ec4e529b28cf733540cf937a6d95
Avi Levin <avilevin@microsoft.com>
2025-04-14 22:33:02 +0300

    Upate buiuld config

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,11 @@ module.exports = {
       inject: 'head'
     })
   ],
-  mode: 'development'
+  mode: 'development',
+  devtool: 'inline-source-map', // Added for better debugging
+  devServer: {
+    static: './dist', // Serve files from the dist directory
+    hot: true, // Enable Hot Module Replacement
+    open: true // Automatically open the browser
+  }
 };
